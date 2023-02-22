4e3c54d62408a3e1262b3c1e695d8efb48d15c34
Quy <quy@broad.dev>
2023-02-22 18:49:29 -0500

    more files

---
+++ src/components/PageFooter.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <a href="https://kp4cd.org/" style="color: #fff !important">
+            Powered by the
+            <span
+                style="
+                    font-weight: 500;
+                    font-size: 1.2em;
+                    vertical-align: -0.1em;
+                "
+                >HuGeAMP</span
+            >
+            <span class="registered-mark">
+                &nbsp;&nbsp;|&nbsp;&nbsp;ACCELERATING MEDICINES PARTNERSHIP and
+                AMP are registered service marks of the U.S. Department of
+                Health and Human Services.
+            </span>
+        </a>
+    </div>
+</template>
+
+<script>
+import Vue from "vue";
+
+export default Vue.component("page-footer", {
+    data() {
+        return {};
+    },
+});
+</script>
