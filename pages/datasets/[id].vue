60fb5b49460a09271cb17fe29ca5f587c17e8b24
Quy <quy@broad.dev>
2023-04-04 09:05:26 -0400

    setup crud ops

--- pages/datasets/[id].vue
+++ pages/datasets/[id].vue
@@ -4,3 +4,31 @@
         <p>Dataset id: {{ $route.params.id }}</p>
     </div>
 </template>
+<script setup>
+//this template is for CRUD operations on a single dataset using id
+//default to fetch data from the server, add form to edit/delete data
+//need to point to proper api endpoints
+
+import { ref, onMounted, onBeforeMount } from "vue";
+const dataset = ref(null);
+
+async function fetchData({ params }) {
+    if (params.id) {
+        const { data } = await $fetch(`/api/dataset/${params.id}`);
+        return { dataset: data };
+    }
+}
+async function deleteDataset() {
+    await $fetch(`/api/dataset/${this.dataset.id}`, {
+        method: "DELETE",
+    });
+    this.$router.push("/datasets");
+}
+async function updateDataset() {
+    await $fetch(`/api/dataset/${this.dataset.id}`, {
+        method: "PUT",
+        body: JSON.stringify(this.dataset),
+    });
+    this.$router.push("/datasets");
+}
+</script>
