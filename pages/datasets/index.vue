50b792f0385efee72abf79cd39c5ec261ce41f3e
Quy <quy@broad.dev>
2023-04-26 16:47:13 -0400

    move dataset list

--- pages/datasets/index.vue
+++ pages/datasets/index.vue
@@ -1,3 +1,29 @@
 <template>
-    <div><h1>List</h1></div>
+    <div class="container">
+        <DataRegistryDatasetTable :data-sets="datasets" />
+    </div>
 </template>
+
+<script setup>
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import DataRegistryDatasetTable from '~/components/DataRegistryDatasetTable.vue'
+const config = useRuntimeConfig();
+const datasets = ref([]);
+onBeforeMount(() => {
+    fetchStudies();
+});
+
+
+async function fetchStudies() {
+    datasets.value = await $fetch(`${config.apiBaseUrl}/api/datasets`, {
+        headers: { "access-token": config.apiSecret },
+    });
+}
+</script>
+
+<style>
+tr {
+    cursor: pointer;
+}
+</style>
