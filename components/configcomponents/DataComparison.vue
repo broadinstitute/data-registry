1df643c238d23271f7c90a8c4296c8974c36adec
Annie Moriondo <amoriond@broadinstitute.org>
2023-06-05 15:02:38 -0400

    component structuring/housekeeping

---
+++ components/configcomponents/DataComparison.vue
@@ -0,0 +1,177 @@
+<template>
+    			<h4>
+				Data comparison
+				<sup class="optional"
+					>Optional for only daynamic data points | Tutorial
+				</sup>
+			</h4>
+			<div class="row">
+				<div class="col-md-12 col">
+					<div>
+						<div title="Builder">
+							<pre />
+							<div class="row dr-builder-ui">
+								<div class="col-md-4 col">
+									<div class="label">Key field</div>
+									<div
+										class="
+											form-check form-check-inline
+										"
+									>
+										<input
+											class="form-check-input"
+											type="radio"
+											value=""
+											id="flexCheckDefault"
+										/>
+										<label
+											class="form-check-label"
+											for="flexCheckDefault"
+										>
+											Variant ID
+										</label>
+									</div>
+									<div
+										class="
+											form-check form-check-inline
+										"
+									>
+										<input
+											class="form-check-input"
+											type="radio"
+											value=""
+											id="flexCheckDefault"
+										/>
+										<label
+											class="form-check-label"
+											for="flexCheckDefault"
+										>
+											P-Value
+										</label>
+									</div>
+								</div>
+								<div class="col-md-4 col">
+									<div class="label">
+										Fields group data key
+										<sup class="optional"
+											>Query parameters
+										</sup>
+									</div>
+									<div
+										class="
+											form-check form-check-inline
+										"
+									>
+										<input
+											class="form-check-input"
+											type="checkbox"
+											value=""
+											id="flexCheckDefault"
+										/>
+										<label
+											class="form-check-label"
+											for="flexCheckDefault"
+										>
+											ancestry
+										</label>
+									</div>
+									<div
+										class="
+											form-check form-check-inline
+										"
+									>
+										<input
+											class="form-check-input"
+											type="checkbox"
+											value=""
+											id="flexCheckDefault"
+										/>
+										<label
+											class="form-check-label"
+											for="flexCheckDefault"
+										>
+											phenotype
+										</label>
+									</div>
+									<div
+										class="
+											form-check form-check-inline
+										"
+									>
+										<input
+											class="form-check-input"
+											type="checkbox"
+											value=""
+											id="flexCheckDefault"
+										/>
+										<label
+											class="form-check-label"
+											for="flexCheckDefault"
+										>
+											region
+										</label>
+									</div>
+								</div>
+								<div class="col-md-4 col">
+									<div class="label">
+										Fields to compare
+									</div>
+									<div
+										class="
+											form-check form-check-inline
+										"
+									>
+										<input
+											class="form-check-input"
+											type="checkbox"
+											value=""
+											id="flexCheckDefault"
+										/>
+										<label
+											class="form-check-label"
+											for="flexCheckDefault"
+										>
+											Variant ID
+										</label>
+									</div>
+									<div
+										class="
+											form-check form-check-inline
+										"
+									>
+										<input
+											class="form-check-input"
+											type="checkbox"
+											value=""
+											id="flexCheckDefault"
+										/>
+										<label
+											class="form-check-label"
+											for="flexCheckDefault"
+										>
+											P-Value
+										</label>
+									</div>
+								</div>
+							</div></div
+						>
+						<div title="Manual build">
+							<pre />
+							<textarea
+								rows="6"
+								class="form-control"
+								>'{\r\n\'key field\':,\r\n\'fields group data key\':[],\r\n\'fields to compare\':[]\r\n}'
+							</textarea>
+						</div>
+					</div>
+				</div>
+			</div>
+</template>
+<style scoped>
+    @import "public/css/mdkp.css";
+    @import "public/css/configbuilder.css";
+</style>
+<script setup>
+	import "bootstrap/dist/css/bootstrap.min.css";
+	import "bootstrap-icons/font/bootstrap-icons.css";
+</script>
---
+++ components/configcomponents/LocusFieldStarColumn.vue
@@ -0,0 +1,54 @@
+<template>
+    <h5>
+        Locus field / star column
+										<sup class="optional">Tutorial</sup>
+									</h5>
+									<div class="row dr-builder-ui">
+										<div
+											class="
+												col-md-6 col
+												text-center
+											"
+										>
+											<div class="label">
+												Locus field
+											</div>
+											<select
+												class="form-control"
+											>
+												<option>None</option>
+												<option>
+													Variant ID
+												</option>
+												<option>P-Value</option>
+											</select>
+										</div>
+										<div
+											class="
+												col-md-6 col
+												text-center
+											"
+										>
+											<div class="label">
+												Star column
+											</div>
+											<select
+												class="form-control"
+											>
+												<option>None</option>
+												<option>
+													Variant ID
+												</option>
+												<option>P-Value</option>
+											</select>
+										</div>
+									</div>
+</template>
+<style scoped>
+    @import "public/css/mdkp.css";
+    @import "public/css/configbuilder.css";
+</style>
+<script setup>
+	import "bootstrap/dist/css/bootstrap.min.css";
+	import "bootstrap-icons/font/bootstrap-icons.css";
+</script>
