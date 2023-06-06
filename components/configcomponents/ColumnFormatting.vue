2e3b5b10204c3dce897333a47c9d6e670f837fb6
Annie Moriondo <amoriond@broadinstitute.org>
2023-06-06 12:19:38 -0400

    hot updates number of decimal places

--- components/configcomponents/ColumnFormatting.vue
+++ components/configcomponents/ColumnFormatting.vue
@@ -21,19 +21,41 @@
                     </li>
                 </ul>
             </div>
-		<div class="col-md-3 col">
+		<div class="col-md-2 col">
 			<div class="label">
 				Select formatting options
 			</div>
 			<ul class="dr-byor-data-columns">
-				<li v-for="formatOption in formatOptions" class="form-check form-check-inline">
-                            <input class="form-check-input" type="checkbox" :value="formatOption.option" 
+				<li v-for="formatOption in Object.keys(formatOptions)" class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" :value="formatOption" 
                                 id="flexCheckDefault" v-model="selectedOptions"/>
-                            <span class="form-check-label" for="flexCheckDefault">{{ formatOption.display }}</span>
+                            <span class="form-check-label" for="flexCheckDefault">{{ formatOptions[formatOption] }}</span>
                     </li>
 			</ul>
 		</div>
-		<div class="col-md-3 col">	
+		<div class="col-md-2 col">
+			<div class="label">
+				Selected options | Change formatting order
+			</div>
+			<tbody class="dr-byor-data-columns">
+				<tr v-for="option, index in selectedOptions" class="arrow-button-list">
+					{{ formatOptions[option] }}
+					<td class="arrow-button-holder">
+						<button class="btn btn-primary arrow-button arrow-button-up" 
+							:disabled="index == 0" @click="moveUp(index)">
+							&uarr;
+						</button>
+					</td>
+					<td class="arrow-button-holder">
+						<button class="btn btn-primary arrow-button"
+						:disabled="index == selectedOptions.length - 1" @click="moveDown(index)">
+						&darr;
+						</button>
+					</td>
+				</tr>
+			</tbody>
+		</div>
+		<div class="col-md-2 col">	
 			<div v-if="selectedOptions.includes('link')" id="linkFormat">
 				<div class="label">
 					Link
@@ -115,44 +137,26 @@
 			</div>
 			<div v-if="selectedOptions.includes('render background percent')">
 				<div class="label">
-				Render background %
+					Render background %
+				</div>
+				<div class="form-inline">
+					<span>Percent if no value:</span>
+					<input type="text" class="form-control form-control-sm input-default"
+						style="width: 50%; margin-left: 5px;"/>
+				</div>
 			</div>
-			<div class="form-inline">
-				<span>Percent if no value:</span>
-				<input type="text" class="form-control form-control-sm input-default"
-					style="width: 50%; margin-left: 5px;"/>
+			<div v-if="selectedOptions.includes('fixed')">
+				<div class="label">
+				Fixed after decimal point
 			</div>
+				<div class="form-inline">
+					<span>Place </span>
+					<select class="form-control form-control-sm" v-model="fixedPlaces"
+							style=" width: 50%; margin-left: 5px;">
+						<option v-for="i in 9">{{ i + 1 }}</option>
+					</select>
+				</div>
 			</div>
-			
-													<pre />
-													<div class="label">
-														Fixed after decimal
-														point
-													</div>
-													<div class="form-inline">
-														<span>Place </span>
-
-														<select
-															class="
-																form-control
-																form-control-sm
-															"
-															style="
-																width: 50%;
-																margin-left: 5px;
-															"
-														>
-															<option>1</option>
-															<option>2</option>
-															<option>3</option>
-															<option>4</option>
-															<option>5</option>
-															<option>6</option>
-															<option>7</option>
-															<option>8</option>
-															<option>9</option>
-														</select>
-													</div>
 		</div>
 		<div class="col-md-1 col">
 													<button
@@ -213,33 +217,21 @@
     const availableFields = computed(()=> props.fields);
     const fieldNameOld = computed(() => props.fieldNameUpdate[0]);
     const fieldNameNew = computed(() => props.fieldNameUpdate[1]);
-	const formatOptions = [
-		{
-			option: "link",
-			display: "Link"
-		},
-		{
-			option: "js math",
-			display: "JavaScript math"
-		},
-		{
-			option: "scientific notation",
-			display: "Scientific notation"
-		},
-		{
-			option: "fixed",
-			display: "Fixed decimal (2-10 places)"
-		},
-		{
-			option: "direction triangle",
-			display: "Direction triangle"
-		},
-		{
-			option: "render background percent",
-			display: "Render background %"
-		}];
+	const formatOptions = {
+		"link": "Link",
+		"js math": "JavaScript math",
+		"scientific notation": "Scientific notation",
+		"fixed": "Fixed decimal",
+		"direction triangle": "Direction triangle",
+		"render background percent": "Render background %",
+		"render background percent negative": "Render background % (negative)"
+	}
 	const selectedColumn = ref(null);
 	const selectedOptions = ref([]);
+	const fixedPlaces = ref(2);
+	const selectedOptionsMod = computed(()=> selectedOptions.value.map(
+			item => item == "fixed" ? `fixed ${fixedPlaces.value}` : item
+		));
 	const singleColumnConfig = ref({
 		"type": []
 	});
@@ -247,7 +239,25 @@
 	const allColumnsConfig = ref({});
 	const allColumnsConfigString = computed(() => JSON.stringify(allColumnsConfig.value));
 	// make sure clicking a bubble is the same as clicking edit
-	watch(selectedOptions, ()=>{
-		singleColumnConfig.value["type"] = selectedOptions.value;
+	watch([selectedOptions, fixedPlaces], ()=>{
+		singleColumnConfig.value["type"] = selectedOptionsMod.value;
 	});
+	function moveUp(index){
+		let beginning = selectedOptions.value.slice(0, index-1);
+		let risingItem = selectedOptions.value[index];
+		let fallingItem = selectedOptions.value[index-1]
+		let end = selectedOptions.value.slice(index + 1);
+		beginning.push(risingItem);
+		beginning.push(fallingItem);
+		selectedOptions.value = beginning.concat(end);
+	}
+	function moveDown(index){
+		let beginning = selectedOptions.value.slice(0, index);
+		let risingItem = selectedOptions.value[index+1];
+		let fallingItem = selectedOptions.value[index]
+		let end = selectedOptions.value.slice(index + 2);
+		beginning.push(risingItem);
+		beginning.push(fallingItem);
+		selectedOptions.value = beginning.concat(end);
+	}
 </script>
