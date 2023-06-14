9e25c58204c7a475de181a823a5ce07954bd4a80
Annie Moriondo <amoriond@broadinstitute.org>
2023-06-14 14:50:20 -0400

    outputting configuration

--- components/configcomponents/Filters.vue
+++ components/configcomponents/Filters.vue
@@ -26,26 +26,8 @@
 										<div class="label">
 											Filter type
 										</div>
-
-										<select
-											class="form-control"
-										>
-											<option>Search</option>
-											<option>
-												Search greater than
-											</option>
-											<option>
-												Search lower than
-											</option>
-											<option>
-												Search or
-											</option>
-											<option>
-												Search and
-											</option>
-											<option>
-												Dropdown
-											</option>
+										<select class="form-control" v-model="selectedFilterType">
+											<option v-for="item in filterTypeOptions">{{ item }}</option>
 										</select>
 									</div>
 									<div class="col-md-3 col">
@@ -53,117 +35,70 @@
 											Label
 										</div>
 										<input type="text" class="form-control input-default" v-model="filterLabel"/>
-												</div>
-												<div class="col-md-3 col">
-													<button
-														class="
-															btn
-															btn-primary
-															btn-sm
-														"
-														type="button"
-													>
-														Save
-													</button>
-													<button
-														class="
-															btn
-															btn-warning
-															btn-sm
-														"
-														type="button"
-													>
-														Cancel
-													</button>
-													<button
-														class="
-															btn
-															btn-danger
-															btn-sm
-														"
-														type="button"
-													>
-														Delete
-													</button>
-												</div>
-											</div>
-										</div>
-										<div class="col-md-5">
-											<div class="row dr-builder-ui">
-												<div class="col-md-6 col">
-													<div class="label">
-														Filter order
-													</div>
-
-													<ul
-														class="
-															dr-byor-data-columns
-														"
-													>
-														<li>
-															Variant ID (<a
-																href="javascript:;"
-																>Edit</a
-															>)<label>
-																<select
-																	class="
-																		form-control
-																	"
-																>
-																	<option>
-																		0
-																	</option>
-																	<option>
-																		1
-																	</option>
-																</select></label
-															>
-														</li>
-														<li>
-															P-Value (<a
-																href="javascript:;"
-																>Edit</a
-															>)<label>
-																<select
-																	class="
-																		form-control
-																	"
-																>
-																	<option>
-																		0
-																	</option>
-																	<option>
-																		1
-																	</option>
-																</select></label
-															>
-														</li>
-													</ul>
-												</div>
-												<div class="col-md-6 col">
-													<div class="label">
-														Filter width
-													</div>
-
-													<select
-														class="form-control"
-													>
-														<option>
-															Midium (default)
-														</option>
-														<option>Small</option>
-														<option>Large</option>
-													</select>
-												</div>
-											</div>
-										</div>
+										<label>
+											<input type="checkbox" v-model="labelInBubble"/>
+											Label in bubble
+										</label>
+									</div>
+									<div class="col-md-3 col">
+										<button class="btn btn-primary btn-sm" type="button" @click="saveFilter()">
+											Save
+										</button>
+										<button class="btn btn-warning btn-sm" type="button">
+											Cancel
+										</button>
+										<button class="btn btn-danger btn-sm" type="button">
+											Delete
+										</button>
+										<div class="failed-save">{{ message }}</div>
 									</div>
 								</div>
-								<div class="label">Output</div>
-								<pre>{{ JSON.stringify(singleFilterConfig)}}</pre>
+							</div>
+							<div class="col-md-5">
+								<div class="row dr-builder-ui">
+									<div class="col-md-6 col">
+										<div class="label">
+											Filter order
+										</div>
+										<ul class="dr-byor-data-columns">
+											<li>Variant ID (<a>Edit</a>)
+												<label>
+													<select class="form-control">
+														<option>0</option>
+														<option>1</option>
+													</select>
+												</label>
+											</li>
+											<li>P-Value (<a>Edit</a>)
+												<label>
+													<select class="form-control">
+														<option>0</option>
+														<option>1</option>
+													</select>
+												</label>
+											</li>
+										</ul>
+									</div>
+									<div class="col-md-6 col">
+										<div class="label">
+											Filter width
+										</div>
+										<select class="form-control">
+											<option>Midium (default)</option>
+											<option>Small</option>
+											<option>Large</option>
+										</select>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
+					<div class="label">Output</div>
+					<pre>{{ JSON.stringify(singleFilterConfig)}}</pre>
+					<pre>{{ JSON.stringify(allFilters) }}</pre>
+				</div>
+			</div>
+		</div>
 </template>
 <style scoped>
     @import "public/css/mdkp.css";
@@ -172,19 +107,31 @@
 <script setup>
     import "bootstrap/dist/css/bootstrap.min.css";
 	import "bootstrap-icons/font/bootstrap-icons.css";
+import { all } from "axios";
     const props = defineProps({fields: Array, fieldNameUpdate: Array});
     const availableFields = computed(() => props.fields);
     const fieldNameOld = computed(() => props.fieldNameUpdate[0]);
     const fieldNameNew = computed(() => props.fieldNameUpdate[1]);
+	const message = ref("");
 	const selectedField = ref(null);
+	const selectedFilterType = ref(null);
+	const labelInBubble = ref(false);
 	const filterLabel = ref("");
-	const blankFilterConfig = {
-		"field": "",
+	const singleFilterConfig = ref({
+		"field": null,
 		"label": "",
-		"type": "",
+		"type": null,
 		"label in bubble": "false"
-	}
-	const singleFilterConfig = ref(blankFilterConfig);
+	});
+	const filterTypeOptions = [
+		"Search",
+		"Search greater than",
+		"Search lower than",
+		"Search or",
+		"Search and",
+		"Dropdown"
+	];
+	const allFilters = ref([]);
     watch(availableFields, () => console.log(JSON.stringify(availableFields.value)));
     watch(fieldNameOld, () => {
 		if (selectedField.value == fieldNameOld.value){
@@ -192,5 +139,32 @@
 		}
 	});
 	watch(selectedField, () => singleFilterConfig.value["field"] = selectedField.value);
-	watch(filterLabel, () => singleFilterConfig.value["label"] = filterLabel.value);
+	watch(filterLabel, () => singleFilterConfig.value["label"] = filterLabel.value.trim());
+	watch(selectedFilterType, () => singleFilterConfig.value["type"] = 
+		selectedFilterType.value == null ? "" : selectedFilterType.value.toLowerCase());
+	watch(labelInBubble, () => singleFilterConfig.value["label in bubble"] = `${labelInBubble.value}`);
+	function saveFilter(){
+		if (singleFilterConfig.value["field"] == null){
+			message.value = "Select a field to filter.";
+			return;
+		}
+		if (singleFilterConfig.value["label"] == ""){
+			message.value = "Enter a filter label.";
+			return;
+		}
+		if (singleFilterConfig.value["type"] == null || 
+				singleFilterConfig.value["type"] == ""){
+			message.value = "Select a filter type.";
+			return;
+		}
+		allFilters.value.push(JSON.parse(JSON.stringify(singleFilterConfig.value)));
+		clearAll();
+	}
+	function clearAll(){
+		message.value = "";
+		selectedField.value = null;
+		selectedFilterType.value = null;
+		labelInBubble.value = false;
+		filterLabel.value = "";
+	}
 </script>
