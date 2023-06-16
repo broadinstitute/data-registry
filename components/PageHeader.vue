a92c5b5c4191938c8921edae70305449ceb190ab
Drew Hite <drew@drewhite.org>
2023-06-16 12:56:24 -0400

    fixing links

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -1,18 +1,14 @@
 <template>
     <nav class="navbar navbar-expand-lg p-3 mb-3 border-bottom">
         <div class="container">
-            <a href="/public" class="navbar-brand mr-4"
-                >[ Data Registry <i class="bi bi-r-square-fill"></i> ]</a
-            >
+            <a href="/" class="navbar-brand mr-4">[ Data Registry <i class="bi bi-r-square-fill"></i> ]</a>
 
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a href="/public" class="nav-link px-2 link-secondary">Home</a>
+                    <a href="/" class="nav-link px-2 link-secondary">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a href="/datasets" class="nav-link px-2 link-body-emphasis"
-                        >Datasets</a
-                    >
+                    <a href="/datasets" class="nav-link px-2 link-body-emphasis">Datasets</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a
