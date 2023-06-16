98bfeecb4c6b4a70e4b20c2ea1b0582ac5b6d86c
Drew Hite <drew@drewhite.org>
2023-06-16 12:06:28 -0400

    working on Quy's PR

--- pages/datasets/[id].vue
+++ pages/datasets/[id].vue
@@ -2,6 +2,23 @@
   <div>
     <DRDatasetAccordion :dsId="dsId" :edit-mode="route.query.edit === 'true'"/>
   </div>
+  <div class="row mt-2">
+    <div class="col text-start">
+      <nuxt-link :to="{ name: 'datasets' }">
+        <button type="button" class="btn btn-success">
+          <i class="bi bi-arrow-left"></i>
+          Back to Dataset List
+        </button></nuxt-link>
+    </div>
+    <div class="col text-end">
+      <nuxt-link :to="{ name: 'datasets-new' }">
+        <button type="button" class="btn btn-primary">
+          <i class="bi bi-plus"></i>
+          Add New Dataset
+        </button>
+      </nuxt-link>
+    </div>
+  </div>
 </template>
 
 <script setup>
--- pages/datasets/new.vue
+++ pages/datasets/new.vue
@@ -2,5 +2,22 @@
   <div class="container-fluid">
     <DRDatasetAccordion />
   </div>
+  <div class="row mt-2">
+    <div class="col text-start">
+      <nuxt-link :to="{ name: 'datasets' }">
+        <button type="button" class="btn btn-success">
+          <i class="bi bi-arrow-left"></i>
+          Back to Dataset List
+        </button></nuxt-link>
+    </div>
+    <div class="col text-end">
+      <nuxt-link :to="{ name: 'datasets-new' }">
+        <button type="button" class="btn btn-primary">
+          <i class="bi bi-plus"></i>
+          Add New Dataset
+        </button>
+      </nuxt-link>
+    </div>
+  </div>
 </template>
 
