66effb2bc12e6e6aed224c77465ebf1b13e8fd22
Annie Moriondo <amoriond@broadinstitute.org>
2023-07-20 16:58:50 -0400

    post content component reads the CSRF token

--- components/UserAuth.vue
+++ components/UserAuth.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="hello">
-      <h1>User Login</h1>
-      <div>
+      <h3>User Login</h3>
+      <div v-if="csrf_token != ''">
+        <h4>Welcome, {{ user }}!</h4>
+        <button @click="logout">Logout</button>
+      </div>
+      <div v-else>
           <label>Username:<input v-model="user"/></label>
           <label>Password: <input type="password" v-model="password"/></label>
           <button @click="login">Login</button>
@@ -12,11 +16,12 @@
 <script setup>
   import "bootstrap/dist/css/bootstrap.min.css";
   import "bootstrap-icons/font/bootstrap-icons.css";
+  const emit = defineEmits(["csrf"]);
   const config = useRuntimeConfig();
   const user = ref("");
   const password = ref("");
-  let csrf_token = "";
-  let logout_token = "";
+  const csrf_token = ref("");
+  const logout_token = ref("");
   async function login(){
       let userData = {
           "name": user.value,
@@ -33,7 +38,30 @@
           });
         const responseObject = await response.json();
         console.log(responseObject);
+        if (!!responseObject.csrf_token && !!responseObject.logout_token){
+          csrf_token.value = responseObject.csrf_token;
+          logout_token.value = responseObject.logout_token;
+          emit("csrf", csrf_token.value);
+        } else {
+          user.value = "";
+          password.value = "";
+        }
     }
+  async function logout(){
+    const response = await fetch(
+      `${config.public.apiDrupalUrl}/user/logout?_format=json&token=${logout_token.value}`, 
+          {
+            method: "POST",
+            body: "{}",
+            withCredentials: true,
+            headers: {
+              "Content-Type": "application/json",
+              "credentials": "include"
+            }
+          });
+        const responseObject = await response.json();
+        console.log(responseObject);
+  }
   </script>
   
   <!-- Add "scoped" attribute to limit CSS to this component only -->
--- pages/create/index.vue
+++ pages/create/index.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="hello">
+    <UserAuth @csrf="token => storeToken(token)"></UserAuth>
     <PostNewContent></PostNewContent>
   </div>
 </template>
+<script setup>
+  const csrf_token = ref("");
+  function storeToken(newCsrfToken){
+    csrf_token.value = newCsrfToken;
+    console.log(`Index page has received token:${csrf_token.value}`);
+  }
+</script>
