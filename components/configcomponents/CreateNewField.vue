eeada54bdf7cc648b4170d58209c5afba16a2d58
Annie Moriondo <amoriond@broadinstitute.org>
2023-08-22 16:23:00 -0400

    hides convert field option when a field is unavailable

--- components/configcomponents/CreateNewField.vue
+++ components/configcomponents/CreateNewField.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="label">
+    <div class="label" v-if="!fieldUnavailable">
             <input class="form-check-input" type="radio" name="first" value="convert" 
                 id="true-button" v-model="convertOrCreate"/>
             Convert "{{ selectedFieldColName }}"
     </div>
-    <div class="label">
+    <div class="label" v-if="!fieldUnavailable">
             <input class="form-check-input" type="radio" name="first" value="create" 
                 id="false-button" v-model="convertOrCreate"/>
             Create new field
@@ -29,6 +29,15 @@
     const createNewField = computed(() => convertOrCreate.value == "create");
     const selectedField = computed(()=> props.selectedField);
     const selectedFieldColName = computed(() => store.getSelectedColumns[selectedField.value]);
+    const unConvertedFields = computed(() => store.getUnConvertedFieldsConfig.map(field => field["raw field"]));
+    const fieldUnavailable = computed(() => {
+        console.log(JSON.stringify(unConvertedFields.value));
+        let unavailable = !unConvertedFields.value.includes(selectedField.value);
+        if (unavailable){
+            convertOrCreate.value = "create";
+        }
+        return unavailable;
+    });
     const newFieldName = ref(!!props.loadedFieldCreateNew ? props.loadedFieldName : "");
     function emitNewName(){
         let nameToEmit = !!createNewField.value ? newFieldName.value : selectedFieldColName.value;
