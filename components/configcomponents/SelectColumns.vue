1d2cd87666b81b5ce0b086c4b5c7ae2a360be7d8
Annie Moriondo <amoriond@broadinstitute.org>
2023-08-29 17:50:55 -0400

    updated formatting for select columns

--- components/configcomponents/SelectColumns.vue
+++ components/configcomponents/SelectColumns.vue
@@ -2,38 +2,30 @@
     Select fields to build page 
     <sup class="optional">Edit default field names to set new names</sup>
     <div class="row">
-        <div class="col-md-1">
-            <button class="btn btn-primary" @click="() => selectAll(true)">Select all</button>
-        </div>
-        <div class="col-md-1">
-            <button class="btn btn-primary" @click="() => selectAll(false)">Clear all</button>
-        </div>
-    </div>
-    <div>
-        <div class="row">
-            <div class="col-md-6 col">
-                <div class="label">Field</div>
-            </div>
-            <div class="col-md-6 col">
-                <div class="label">New field name</div>
-            </div>
-        </div>
-        <div v-for="rawField, index in rawFields" class="row">
-            <div class="col-md-6 col">
-                <input class="form-check-input" type="checkbox" id="flexCheckDefault" 
-                    :value="rawField" v-model="selectedColumns"
-                    @change="toggleBox(index)"/>
-                <span class="form-check-label" for="flexCheckDefault">{{ rawField }}</span>
-            </div>
-            <div class="col-md-4 col">
-                <input class="column-select form-control input-default" 
-                    :disabled="!selectedColumns.includes(rawField)" :id="`field_${index}`" type="text" 
-                    @change="event => setColumnName(index, event.target.value)"/>
-                <div :id="`warning_${index}`" hidden>
-                    <sup>Field names must be unique</sup>
-                </div>
-            </div>
-		</div>
+        <table class="col-md-10">
+            <tr v-if="rawFields.length > 0">
+                <th>
+                    <input class="form-check-input" type="checkbox" 
+                        v-model="selectAllBox" @change="toggleSelectAll()"/>
+                </th>
+                <th>Field</th>
+                <th>New field name</th>
+            </tr>
+            <tr v-for="rawField, index in rawFields">
+                <td>
+                    <input class="form-check-input" type="checkbox" id="flexCheckDefault" 
+                        :value="rawField" v-model="selectedColumns"
+                        @change="toggleBox(index)"/>
+                </td>
+                <td>{{ rawField }}</td>
+                <td>
+                    <input class="column-select form-control input-default" 
+                        :disabled="!selectedColumns.includes(rawField)" :id="`field_${index}`" type="text" 
+                        @change="event => setColumnName(index, event.target.value)"/>
+                    <sup :id="`warning_${index}`" hidden>Field names must be unique</sup>
+                </td>
+            </tr>
+        </table>
     </div>
 </template>
 <script setup>
@@ -42,6 +34,7 @@
     const store = useConfigBuilderStore();
     const rawFields = computed(()=> store.rawFields);
     const selectedColumns = ref([]);
+    const selectAllBox = ref(false);
     function toggleBox(index){
         let rawField = rawFields.value[index];
         if(selectedColumns.value.includes(rawField)){
@@ -70,7 +63,7 @@
     function restoreDefaultColumnName(index){
         let rawField = rawFields.value[index];
         setDisplayName(index, rawField);
-        store.renameColumn(rawField, newName);
+        store.renameColumn(rawField, rawField);
         //What happens if the original column data has an empty string in it?
     }
 
@@ -78,8 +71,8 @@
         let inputField = document.querySelector(`input#field_${index}`);
         inputField.value = newName;
     }
-    function selectAll(toggleOn){
-        selectedColumns.value = !!toggleOn ? rawFields.value : [];
+    function toggleSelectAll(){
+        selectedColumns.value = !!selectAllBox.value ? rawFields.value : [];
         for (let i = 0; i < rawFields.value.length; i++){
             toggleBox(i);
         }
