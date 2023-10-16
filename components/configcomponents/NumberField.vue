220e8181dd04cb213da10c07c592e20fedc95c03
Annie Moriondo <amoriond@broadinstitute.org>
2023-10-16 16:51:09 -0400

    this is where the number field is for now

--- components/configcomponents/NumberField.vue
+++ components/configcomponents/NumberField.vue
@@ -10,32 +10,14 @@
         "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "Backspace", "e", ".", "-"
     ];
     function numericInput(event){
-        let inputKey = event.key;
-        if (inputKey === "Backspace"){
-            return;
-        }
-        let notionalNumber = parseFloat(`${numberValue.value.toString()}${inputKey}`);
-        console.log(notionalNumber);
-        if (Number.isNaN(notionalNumber)){
+        if (characterNotLicit(event.key)){
             event.preventDefault();
         }
     }
     function characterNotLicit(key){
         return !NUMBER_KEYS.includes(key);
     }
-    function disallowedNumberType(key){
-        return (!!props.integerOnly && (key === "e" || key === "."));
-    }
-    function disallowedSign(key){
-        return (!!props.positiveOnly && key === "-");
-    }
-    function duplicateE(key){
-        console.log(numberValue.value);
-        return (key === "e" && numberValue.value.toString().includes("e"));
-    }
-    function duplicateMinus(key){
-        return key === "-" && numberValue.value.toString().includes("-");
-    }
+   // What to do about bad input like starting with an e?
     watch(numberValue, () => {
         let floatValue = parseFloat(numberValue.value);
         floatValue = typeof floatValue === "number" ? floatValue : null;
