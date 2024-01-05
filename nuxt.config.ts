ee9ab42e8e0466c073341fe8171aabb145cee1fd
Drew Hite <drew@drewhite.org>
2024-01-04 19:16:06 -0500

    should run now

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
       phenotypesUrl: "",
     },
   },
-  modules: ["@nuxt/devtools", "@pinia/nuxt", "@nuxtjs/eslint-module"],
+  modules: ["@nuxt/devtools", "@pinia/nuxt"],
   // https://stackoverflow.com/questions/74003458/cannot-find-module-pinia-dist-pinia-mjs-when-using-run-dev
   alias: {
     pinia: "/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs"
