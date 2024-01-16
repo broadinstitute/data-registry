7127529f1d2682affd33e4eb92b599622c99758d
Annie Moriondo <amoriond@broadinstitute.org>
2024-01-16 16:53:24 -0500

    getting ready to place buttons

--- components/configcomponents/visualizers/Heatmap.vue
+++ components/configcomponents/visualizers/Heatmap.vue
@@ -1,150 +1,239 @@
 <template>
   <div class="row">
     <div class="col-md-2" id="leftFieldWrapper">
-      <div v-if="editingFieldset === 'heatmap-plot-label'">
-        <tbody class="pad-field">
-          <tr>
-            <td class="popup-field-label">
-              Plot label:
-            </td>
-            <td>
-              <input v-model="plotLabel" type="text" class="form-control input-default form-control-sm">
-            </td>
-          </tr>
-        </tbody>
-      </div>
-      <div v-else-if="editingFieldset === 'heatmap-column'">
-        <tbody>
-          <tr>
-            <td class="popup-field-label">
-              Column field:
-            </td>
-            <td>
-              <select v-model="columnField" class="form-control form-control-sm">
-                <option value="">
-                  Select a field
-                </option>
-                <option v-for="field in availableFields" :key="field">
-                  {{ field }}
-                </option>
-              </select>
-            </td>
-          </tr>
-          <tr>
-            <td class="popup-field-label">
-              Column label:
-            </td>
-            <td>
-              <input
-                  v-model="columnLabel"
+      <div id="leftField" v-if="editingFieldset !== ''">
+        <div v-if="editingFieldset === 'heatmap-plot-label'">
+          <tbody class="pad-field">
+            <tr>
+              <td class="popup-field-label">
+                Plot label:
+              </td>
+              <td>
+                <input v-model="plotLabel" type="text" class="form-control input-default form-control-sm">
+              </td>
+            </tr>
+          </tbody>
+        </div>
+        <div v-else-if="editingFieldset === 'heatmap-column'">
+          <tbody>
+            <tr>
+              <td class="popup-field-label">
+                Column field:
+              </td>
+              <td>
+                <select v-model="columnField" class="form-control form-control-sm">
+                  <option value="">
+                    Select a field
+                  </option>
+                  <option v-for="field in availableFields" :key="field">
+                    {{ field }}
+                  </option>
+                </select>
+              </td>
+            </tr>
+            <tr>
+              <td class="popup-field-label">
+                Column label:
+              </td>
+              <td>
+                <input
+                    v-model="columnLabel"
+                    type="text"
+                    class="form-control input-default form-control-sm"
+                  >
+              </td>
+            </tr>
+          </tbody>
+        </div>
+        <div v-else-if="editingFieldset === 'heatmap-row'">
+          <tbody>
+            <tr>
+              <td class="popup-field-label">
+                Row field:
+              </td>
+              <td>
+                <select v-model="columnField" class="form-control form-control-sm">
+                  <option value="">
+                    Select a field
+                  </option>
+                  <option v-for="field in availableFields" :key="field">
+                    {{ field }}
+                  </option>
+                </select>
+              </td>
+            </tr>
+            <tr>
+              <td class="popup-field-label">
+                Row label:
+              </td>
+              <td>
+                <input
+                    v-model="rowLabel"
+                    type="text"
+                    class="form-control input-default form-control-sm"
+                  >
+              </td>
+            </tr>
+          </tbody>
+        </div>
+        <div v-else-if="editingFieldset === 'heatmap-main'">
+          <tbody>
+            <tr>
+              <td class="popup-field-label">
+                Field:
+              </td>
+              <td>
+                <select v-model="mainField" class="form-control form-control-sm">
+                  <option value="">
+                    Select a field
+                  </option>
+                  <option v-for="field in availableFields" :key="field">
+                    {{ field }}
+                  </option>
+                </select>
+              </td>
+            </tr>
+            <tr>
+              <td class="popup-field-label">
+                Label:
+              </td>
+              <td>
+                <input
+                  v-model="mainLabel"
                   type="text"
                   class="form-control input-default form-control-sm"
                 >
-            </td>
-          </tr>
-        </tbody>
-      </div>
-      <div v-else-if="editingFieldset === 'heatmap-row'">
-        <tbody>
-          <tr>
-            <td class="popup-field-label">
-              Row field:
-            </td>
-            <td>
-              <select v-model="columnField" class="form-control form-control-sm">
-                <option value="">
-                  Select a field
-                </option>
-                <option v-for="field in availableFields" :key="field">
-                  {{ field }}
-                </option>
-              </select>
-            </td>
-          </tr>
-          <tr>
-            <td class="popup-field-label">
-              Row label:
-            </td>
-            <td>
-              <input
-                  v-model="rowLabel"
+              </td>
+            </tr>
+            <!-- "Render type" param - only current option is 'scale'-->
+            <tr>
+              <td class="popup-field-label">
+                Direction:
+              </td>
+              <td>
+                <select v-model="mainDirection" class="form-control form-control-sm">
+                  <option value="positive">
+                    Positive (higher darker)
+                  </option>
+                  <option value="negative">
+                    Negative (lower darker)
+                  </option>
+                </select>
+              </td>
+            </tr>
+            <tr>
+              <td class="popup-field-label">
+                Low:
+              </td>
+              <td>
+                <input v-model="mainLow" class="form-control form-control-sm" type="number">
+              </td>
+            </tr>
+            <tr>
+              <td class="popup-field-label">
+                Mid:
+              </td>
+              <td>
+                <input v-model="mainMid" class="form-control form-control-sm" type="number">
+              </td>
+            </tr>
+            <tr>
+              <td class="popup-field-label">
+                High:
+              </td>
+              <td>
+                <input v-model="mainHigh" class="form-control form-control-sm" type="number">
+              </td>
+            </tr>
+          </tbody>
+        </div>
+        <div v-else-if="editingFieldset === 'heatmap-sub'">
+          <tbody>
+            <tr>
+              <td class="popup-field-label">
+                Field
+              </td>
+              <td>
+                <select v-model="subField" class="form-control form-control-sm">
+                  <option value="">
+                    None
+                  </option>
+                  <option v-for="field in availableFields" :value="field" :key="field">
+                    {{ field }}
+                  </option>
+                </select>
+              </td>
+            </tr>
+          </tbody>
+          <tbody v-if="subField !== ''">
+            <tr>
+              <td class="popup-field-label">
+                Label:
+              </td>
+              <td>
+                <input
+                  v-model="subLabel"
                   type="text"
                   class="form-control input-default form-control-sm"
                 >
-            </td>
-          </tr>
-        </tbody>
-      </div>
-      <div v-else-if="editingFieldset === 'heatmap-main'">
-        <tbody>
-          <tr>
-            <td class="popup-field-label">
-              Field:
-            </td>
-            <td>
-              <select v-model="mainField" class="form-control form-control-sm">
-                <option value="">
-                  Select a field
-                </option>
-                <option v-for="field in availableFields" :key="field">
-                  {{ field }}
-                </option>
-              </select>
-            </td>
-          </tr>
-          <tr>
-            <td class="popup-field-label">
-              Label:
-            </td>
-            <td>
-              <input
-                v-model="mainLabel"
-                type="text"
-                class="form-control input-default form-control-sm"
-              >
-            </td>
-          </tr>
-          <!-- "Render type" param - only current option is 'scale'-->
-          <tr>
-            <td class="popup-field-label">
-              Direction:
-            </td>
-            <td>
-              <select v-model="mainDirection" class="form-control form-control-sm">
-                <option value="positive">
-                  Positive (higher darker)
-                </option>
-                <option value="negative">
-                  Negative (lower darker)
-                </option>
-              </select>
-            </td>
-          </tr>
-          <tr>
-            <td class="popup-field-label">
-              Low:
-            </td>
-            <td>
-              <input v-model="mainLow" class="form-control form-control-sm" type="number">
-            </td>
-          </tr>
-          <tr>
-            <td class="popup-field-label">
-              Mid:
-            </td>
-            <td>
-              <input v-model="mainMid" class="form-control form-control-sm" type="number">
-            </td>
-          </tr>
-          <tr>
-            <td class="popup-field-label">
-              High:
-            </td>
-            <td>
-              <input v-model="mainHigh" class="form-control form-control-sm" type="number">
-            </td>
-          </tr>
-        </tbody>
+              </td>
+            </tr>
+            <!-- Only render type currently available is 'steps'-->
+            <tr>
+              <td class="popup-field-label">
+                Direction:
+              </td>
+              <td>
+                <select v-model="subDirection" class="form-control form-control-sm">
+                  <option value="positive">
+                    Positive (higher bigger)
+                  </option>
+                  <option value="negative">
+                    Negative (lower bigger)
+                  </option>
+                </select>
+              </td>
+            </tr>
+            <tr>
+              <td colspan="2" class="popup-field-label">
+                Value steps:
+              </td>
+            </tr>
+            <tr v-for="(_, index) in subSteps" :key="index">
+              <td>
+                <input
+                  v-model="subSteps[index]"
+                  class="form-control form-control-sm"
+                  type="number"
+                >
+              </td>
+              <td>
+                <button
+                  v-if="subSteps.length > 1"
+                  class="btn btn-secondary replace-chars-button delete-button"
+                  @click="subSteps.splice(index, 1)"
+                >
+                  &times;
+                </button>
+              </td>
+            </tr>
+          </tbody>
+          <button class="btn btn-primary add-button" @click="subSteps.push('')">
+            Add
+          </button>
+        </div>
+        <div v-else-if="editingFieldset === 'heatmap-font-size'">
+          <tbody>
+            <tr>
+              <td class="popup-field-label">
+                Font size:
+              </td>
+              <td>
+                <input v-model="fontSize" class="form-control form-control-sm" type="number">
+              </td>
+            </tr>
+          </tbody>
+        </div>
       </div>
     </div>
     <div id="heatmap-gui" class="col-md-9 viz-gui">
@@ -166,105 +255,24 @@
         <span class="item-undone" hidden>&#9888;</span>
         Plot label
       </button>
+      <button id="heatmap-font-size" class="btn btn-primary btn-sm gui-btn" @click="editFieldset('heatmap-font-size')">
+        <span class="pencil">&#9998;</span>
+        <span class="item-done" hidden>&check;</span>
+        <span class="item-undone" hidden>&#9888;</span>
+        Font size
+      </button>
       <button id="heatmap-main" class="btn btn-primary btn-sm gui-btn" @click="editFieldset('heatmap-main')">
         <span class="pencil">&#9998;</span>
         <span class="item-done" hidden>&check;</span>
         <span class="item-undone" hidden>&#9888;</span>
         Box color coding
       </button>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-md-2">
-      Font size<sup class="required"> *</sup>
-    </div>
-    <div class="col-md-4">
-      <input v-model="fontSize" class="form-control form-control-sm" type="number">
-    </div>
-  </div>
-  <div class="label">
-    Sub circle scaling
-  </div>
-  <div class="row">
-    <div class="col-md-2">
-      Field
-    </div>
-    <div class="col-md-4">
-      <select v-model="subField" class="form-control form-control-sm">
-        <option value="">
-          None
-        </option>
-        <option v-for="field in availableFields" :value="field" :key="field">
-          {{ field }}
-        </option>
-      </select>
-    </div>
-    <div v-if="subField !== ''" class="col-md-2">
-      Label<sup class="required"> *</sup>
-    </div>
-    <div v-if="subField !== ''" class="col-md-4">
-      <input
-        v-model="subLabel"
-        type="text"
-        class="form-control input-default form-control-sm"
-      >
-    </div>
-  </div>
-  <div v-if="subField !== ''">
-    <div class="row">
-      <div class="col-md-2">
-        Render type<sup class="required"> *</sup>
-      </div>
-      <div class="col-md-4">
-        <select v-model="subRenderType" class="form-control form-control-sm">
-          <option value="steps">
-            Steps
-          </option>
-        </select>
-      </div>
-      <div class="col-md-2">
-        Direction<sup class="required"> *</sup>
-      </div>
-      <div class="col-md-4">
-        <select v-model="subDirection" class="form-control form-control-sm">
-          <option value="positive">
-            Positive (higher bigger)
-          </option>
-          <option value="negative">
-            Negative (lower bigger)
-          </option>
-        </select>
-      </div>
-    </div>
-    <div v-if="subRenderType == 'steps'" class="row">
-      <div class="col-md-2">
-        Value steps<sup class="required"> *</sup>
-      </div>
-      <div class="col-md-4 col">
-        <table>
-          <tr v-for="(_, index) in subSteps" :key="index">
-            <td>
-              <input
-                v-model="subSteps[index]"
-                class="form-control form-control-sm"
-                type="number"
-              >
-            </td>
-            <td>
-              <button
-                v-if="subSteps.length > 1"
-                class="btn btn-secondary replace-chars-button delete-button"
-                @click="subSteps.splice(index, 1)"
-              >
-                &times;
-              </button>
-            </td>
-          </tr>
-        </table>
-        <button class="btn btn-primary add-button" @click="subSteps.push('')">
-          Add
-        </button>
-      </div>
+      <button id="heatmap-sub" class="btn btn-primary btn-sm gui-btn" @click="editFieldset('heatmap-sub')">
+        <span class="pencil">&#9998;</span>
+        <span class="item-done" hidden>&check;</span>
+        <span class="item-undone" hidden>&#9888;</span>
+        Sub circle scaling
+      </button>
     </div>
   </div>
 </template>
