7a651b31125846289fc19bef1625962b20b26673
Drew Hite <drew@drewhite.org>
2024-02-08 15:17:56 -0500

    add vue directive for permissions, improve communication from server to client

---
+++ plugins/directives.js
@@ -0,0 +1,18 @@
+import { useUserStore } from '~/stores/UserStore';
+
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.directive('can', {
+    mounted: (el, binding) => {
+      const user = useUserStore().user;
+      if(user.roles.includes('admin')) {
+        return;
+      }
+      const { value } = binding;
+      if (!user.permissions.includes(value)) {
+        el.parentNode.removeChild(el);
+      }
+    }
+  });
+});
+
