1d6764e99046f3f44833ecbda1d6176e3cadf820
Drew Hite <drew@drewhite.org>
2024-07-15 14:06:44 -0400

    adding compatibility date

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,80 +1,90 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    ssr: false,
-    devtools: {
-        enabled: true,
+  ssr: false,
 
-        timeline: {
-            enabled: true,
-        },
-    },
-    app: {
-        head: {
-            title: "KPN Data Registry",
-        },
-    },
-    css: [
-        //"bootstrap/dist/css/bootstrap.css",
-        "bootstrap-icons/font/bootstrap-icons.css",
-        //"assets/css/styles.css",
-        "primeicons/primeicons.css",
-        "primeflex/primeflex.scss",
-        "primevue/resources/primevue.min.css",
-        "@/assets/styles.scss",
-        // "assets/css/base.css",
-        "primevue/resources/themes/aura-light-indigo/theme.css",
-    ],
+  devtools: {
+      enabled: true,
 
-    runtimeConfig: {
-        public: {
-            skipAuth: false,
-            apiBaseUrl: "",
-            phenotypesUrl: "",
-            googleAuthClientId: "",
-            googleAuthRedirectUri: "",
-        },
-    },
-    modules: [
-        "@pinia/nuxt",
-        "@nuxt/devtools", // "@nuxtjs/tailwindcss",
-        "nuxt-primevue",
-        "nuxt-shiki",
-    ],
-    primevue: {
-        options: { unstyled: false, ripple: true },
-        //importPT: {
-        //    from: "/presets/lara/",
-        //}, //import and apply preset
-        components: {
-            // prefix: "Prime",
-        },
-    },
-    shiki: {
-        bundledLangs: [
-            "javascript",
-            "typescript",
-            "json",
-            "html",
-            "css",
-            "scss",
-            "yaml",
-            "markdown",
-            "r",
-        ],
-    },
+      timeline: {
+          enabled: true,
+      },
+  },
 
-    // https://stackoverflow.com/questions/74003458/cannot-find-module-pinia-dist-pinia-mjs-when-using-run-dev
-    alias: {
-        pinia: "/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs",
-    },
-    telemetry: false,
-    hooks: {
-        'pages:extend'(pages) {
-            // add a route
-            pages.push({
-                path: '/',
-                redirect: '/datasets'
-            });
-        }
-    }
+  app: {
+      head: {
+          title: "KPN Data Registry",
+      },
+  },
+
+  css: [
+      //"bootstrap/dist/css/bootstrap.css",
+      "bootstrap-icons/font/bootstrap-icons.css",
+      //"assets/css/styles.css",
+      "primeicons/primeicons.css",
+      "primeflex/primeflex.scss",
+      "primevue/resources/primevue.min.css",
+      "@/assets/styles.scss",
+      // "assets/css/base.css",
+      "primevue/resources/themes/aura-light-indigo/theme.css",
+  ],
+
+  runtimeConfig: {
+      public: {
+          skipAuth: false,
+          apiBaseUrl: "",
+          phenotypesUrl: "",
+          googleAuthClientId: "",
+          googleAuthRedirectUri: "",
+      },
+  },
+
+  modules: [
+      "@pinia/nuxt",
+      "@nuxt/devtools", // "@nuxtjs/tailwindcss",
+      "nuxt-primevue",
+      "nuxt-shiki",
+  ],
+
+  primevue: {
+      options: { unstyled: false, ripple: true },
+      //importPT: {
+      //    from: "/presets/lara/",
+      //}, //import and apply preset
+      components: {
+          // prefix: "Prime",
+      },
+  },
+
+  shiki: {
+      bundledLangs: [
+          "javascript",
+          "typescript",
+          "json",
+          "html",
+          "css",
+          "scss",
+          "yaml",
+          "markdown",
+          "r",
+      ],
+  },
+
+  // https://stackoverflow.com/questions/74003458/cannot-find-module-pinia-dist-pinia-mjs-when-using-run-dev
+  alias: {
+      pinia: "/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs",
+  },
+
+  telemetry: false,
+
+  hooks: {
+      'pages:extend'(pages) {
+          // add a route
+          pages.push({
+              path: '/',
+              redirect: '/datasets'
+          });
+      }
+  },
+
+  compatibilityDate: "2024-07-15"
 });
