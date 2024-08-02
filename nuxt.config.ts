f3d9cade7661563df02f41d2316d047508edbfab
Quy <quy@broad.dev>
2024-08-02 16:02:53 -0400

    wip - initial

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,90 +1,100 @@
+import Aura from "@primevue/themes/aura";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
+    ssr: false,
 
-  devtools: {
-      enabled: true,
+    devtools: {
+        enabled: true,
 
-      timeline: {
-          enabled: true,
-      },
-  },
+        timeline: {
+            enabled: true,
+        },
+    },
 
-  app: {
-      head: {
-          title: "KPN Data Registry",
-      },
-  },
+    app: {
+        head: {
+            title: "KPN Data Registry",
+        },
+    },
 
-  css: [
-      //"bootstrap/dist/css/bootstrap.css",
-      "bootstrap-icons/font/bootstrap-icons.css",
-      //"assets/css/styles.css",
-      "primeicons/primeicons.css",
-      "primeflex/primeflex.scss",
-      "primevue/resources/primevue.min.css",
-      "@/assets/styles.scss",
-      // "assets/css/base.css",
-      "primevue/resources/themes/aura-light-indigo/theme.css",
-  ],
+    css: [
+        //"bootstrap/dist/css/bootstrap.css",
+        "bootstrap-icons/font/bootstrap-icons.css",
+        //"assets/css/styles.css",
+        "primeicons/primeicons.css",
+        // "primeflex/primeflex.scss",
+        // "primevue/resources/primevue.min.css",
+        "@/assets/styles.scss",
+        // "assets/css/base.css",
+        // "primevue/resources/themes/aura-light-indigo/theme.css",
+    ],
 
-  runtimeConfig: {
-      public: {
-          skipAuth: false,
-          apiBaseUrl: "",
-          phenotypesUrl: "",
-          googleAuthClientId: "",
-          googleAuthRedirectUri: "",
-      },
-  },
+    runtimeConfig: {
+        public: {
+            skipAuth: false,
+            apiBaseUrl: "",
+            phenotypesUrl: "",
+            googleAuthClientId: "",
+            googleAuthRedirectUri: "",
+        },
+    },
 
-  modules: [
-      "@pinia/nuxt",
-      "@nuxt/devtools", // "@nuxtjs/tailwindcss",
-      "nuxt-primevue",
-      "nuxt-shiki",
-  ],
+    modules: [
+        "@pinia/nuxt",
+        "@nuxt/devtools",
+        "@primevue/nuxt-module",
+        "nuxt-shiki",
+    ],
 
-  primevue: {
-      options: { unstyled: false, ripple: true },
-      //importPT: {
-      //    from: "/presets/lara/",
-      //}, //import and apply preset
-      components: {
-          // prefix: "Prime",
-      },
-  },
+    primevue: {
+        options: {
+            unstyled: false,
+            ripple: true,
+            theme: {
+                preset: Aura,
+                options: {
+                    darkModeSelector: ".dark-mode",
+                },
+            },
+        },
+        //importPT: {
+        //    from: "/presets/lara/",
+        //}, //import and apply preset
+        components: {
+            // prefix: "Prime",
+        },
+    },
 
-  shiki: {
-      bundledLangs: [
-          "javascript",
-          "typescript",
-          "json",
-          "html",
-          "css",
-          "scss",
-          "yaml",
-          "markdown",
-          "r",
-      ],
-  },
+    shiki: {
+        bundledLangs: [
+            "javascript",
+            "typescript",
+            "json",
+            "html",
+            "css",
+            "scss",
+            "yaml",
+            "markdown",
+            "r",
+        ],
+    },
 
-  // https://stackoverflow.com/questions/74003458/cannot-find-module-pinia-dist-pinia-mjs-when-using-run-dev
-  alias: {
-      pinia: "/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs",
-  },
+    // https://stackoverflow.com/questions/74003458/cannot-find-module-pinia-dist-pinia-mjs-when-using-run-dev
+    alias: {
+        pinia: "/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs",
+    },
 
-  telemetry: false,
+    telemetry: false,
 
-  hooks: {
-      'pages:extend'(pages) {
-          // add a route
-          pages.push({
-              path: '/',
-              redirect: '/datasets'
-          });
-      }
-  },
+    hooks: {
+        "pages:extend"(pages) {
+            // add a route
+            pages.push({
+                path: "/",
+                redirect: "/datasets",
+            });
+        },
+    },
 
-  compatibilityDate: "2024-07-15"
+    compatibilityDate: "2024-07-15",
 });
