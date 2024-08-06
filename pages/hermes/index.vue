26c9f58b9cd2149700dce11a7039d91fd985bd1a
Quy <quy@broad.dev>
2024-08-06 12:15:04 -0400

    remove extra divs

--- pages/hermes/index.vue
+++ pages/hermes/index.vue
@@ -46,155 +46,135 @@ const getIcon = (status) => {
 </script>
 
 <template>
-    <div class="grid grid-cols-1">
-        <div v-if="fileUploads.length && finished" class="col">
-            <Card>
-                <template #content>
-                    <DataTable
-                        :value="fileUploads"
-                        :paginator="false"
-                        rowHover
-                        :rows="10"
-                        :rowsPerPageOptions="[5, 10, 20]"
-                        :loading="tableLoading"
-                        sortField="uploaded_at"
-                        :sortOrder="-1"
-                        ><template #header>
-                            <div class="flex justify-end flex-col sm:flex-row">
-                                <Button
-                                    type="button"
-                                    label="Upload New Dataset"
-                                    icon="bi-upload"
-                                    outlined
-                                    @click="() => $router.push('/hermes/new')"
+    <Card v-if="fileUploads.length && finished">
+        <template #content>
+            <DataTable
+                :value="fileUploads"
+                :paginator="false"
+                rowHover
+                :rows="10"
+                :rowsPerPageOptions="[5, 10, 20]"
+                :loading="tableLoading"
+                sortField="uploaded_at"
+                :sortOrder="-1"
+                ><template #header>
+                    <div class="flex justify-end flex-col sm:flex-row">
+                        <Button
+                            type="button"
+                            label="Upload New Dataset"
+                            icon="bi-upload"
+                            outlined
+                            @click="() => $router.push('/hermes/new')"
+                        />
+                    </div>
+                </template>
+                <template #empty> No dataset found. </template>
+                <template #loading>
+                    Loading dataset data. Please wait...
+                </template>
+                <Column field="dataset_name" header="Dataset"></Column>
+                <Column field="file_name" header="File Name"></Column>
+                <Column field="file_size" header="File Size">
+                    <template #body="{ data }">
+                        {{
+                            (data.file_size / (1024 * 1024)).toFixed(2) + " mb"
+                        }}
+                    </template>
+                </Column>
+                <Column field="uploaded_at" header="Date Uploaded">
+                    <template #body="{ data }">
+                        {{ formatDate(new Date(data.uploaded_at)) }}
+                    </template>
+                </Column>
+                <Column field="uploaded_by" header="Uploader"></Column>
+                <Column field="phenotype" header="Phenotype"></Column>
+                <Column field="qc_status" header="Status">
+                    <template #body="{ data }">
+                        <span v-if="data.qc_status !== 'SUBMITTED TO QC'">
+                            <nuxt-link :to="`/hermes/qc/${data.id}`">
+                                <Tag
+                                    :severity="getSeverity(data.qc_status)"
+                                    :icon="getIcon(data.qc_status)"
+                                    :value="data.qc_status.toUpperCase()"
+                                    rounded
                                 />
-                            </div>
-                        </template>
-                        <template #empty> No dataset found. </template>
-                        <template #loading>
-                            Loading dataset data. Please wait...
-                        </template>
-                        <Column field="dataset_name" header="Dataset"></Column>
-                        <Column field="file_name" header="File Name"></Column>
-                        <Column field="file_size" header="File Size">
-                            <template #body="{ data }">
-                                {{
-                                    (data.file_size / (1024 * 1024)).toFixed(
-                                        2,
-                                    ) + " mb"
-                                }}
-                            </template>
-                        </Column>
-                        <Column field="uploaded_at" header="Date Uploaded">
-                            <template #body="{ data }">
-                                {{ formatDate(new Date(data.uploaded_at)) }}
-                            </template>
-                        </Column>
-                        <Column field="uploaded_by" header="Uploader"></Column>
-                        <Column field="phenotype" header="Phenotype"></Column>
-                        <Column field="qc_status" header="Status">
-                            <template #body="{ data }">
-                                <span
-                                    v-if="data.qc_status !== 'SUBMITTED TO QC'"
-                                >
-                                    <nuxt-link :to="`/hermes/qc/${data.id}`">
-                                        <Tag
-                                            :severity="
-                                                getSeverity(data.qc_status)
-                                            "
-                                            :icon="getIcon(data.qc_status)"
-                                            :value="
-                                                data.qc_status.toUpperCase()
-                                            "
-                                            rounded
-                                        />
-                                    </nuxt-link>
-                                </span>
-                                <span v-else>
-                                    <Tag
-                                        :severity="getSeverity(data.qc_status)"
-                                        :icon="getIcon(data.qc_status)"
-                                        :value="data.qc_status.toUpperCase()"
-                                        rounded
-                                    />
-                                </span>
-                            </template>
-                        </Column>
-                        <Column field="qc_report" header="QC Report">
-                            <template #body="{ data }">
-                                <NuxtLink :to="`/hermes/qc/${data.id}`">
-                                    <Button
-                                        v-if="
-                                            data.qc_status !== 'SUBMITTED TO QC'
-                                        "
-                                        outlined
-                                        size="small"
-                                    >
-                                        View
-                                    </Button></NuxtLink
-                                >
-                            </template>
-                        </Column>
-                    </DataTable>
-                </template>
-            </Card>
-        </div>
-        <div v-else-if="finished && !fileUploads.length" class="col">
-            <Card>
-                <template #content>
-                    <div
-                        class="bg-surface-0 dark:bg-surface-950 px-6 py-20 md:px-12 lg:px-20"
-                    >
-                        <div
-                            class="text-surface-700 dark:text-surface-100 text-center"
-                        >
-                            <div
-                                class="text-surface-900 dark:text-surface-0 font-bold text-5xl mb-4"
-                            >
-                                Upload your datasets.
-                            </div>
-                            <div
-                                class="text-surface-700 dark:text-surface-100 text-2xl mb-8"
-                            >
-                                You don't have any dataset yet. Start uploading
-                                your first dataset today.
-                            </div>
+                            </nuxt-link>
+                        </span>
+                        <span v-else>
+                            <Tag
+                                :severity="getSeverity(data.qc_status)"
+                                :icon="getIcon(data.qc_status)"
+                                :value="data.qc_status.toUpperCase()"
+                                rounded
+                            />
+                        </span>
+                    </template>
+                </Column>
+                <Column field="qc_report" header="QC Report">
+                    <template #body="{ data }">
+                        <NuxtLink :to="`/hermes/qc/${data.id}`">
                             <Button
-                                label="Upload"
-                                icon="bi-upload"
-                                class="mr-2"
-                                @click="route.push('/hermes/new')"
-                            ></Button>
-                        </div>
-                    </div>
-                </template>
-            </Card>
-        </div>
-        <div v-else class="col">
-            <Card>
-                <template #content>
-                    <div
-                        class="bg-surface-0 dark:bg-surface-950 px-6 py-20 md:px-12 lg:px-20"
-                    >
-                        <div
-                            class="text-surface-700 dark:text-surface-100 text-center"
+                                v-if="data.qc_status !== 'SUBMITTED TO QC'"
+                                outlined
+                                size="small"
+                            >
+                                View
+                            </Button></NuxtLink
                         >
-                            <div
-                                class="text-surface-600 dark:text-surface-200 font-bold text-4xl mb-4"
-                            >
-                                Loading...
-                            </div>
-                            <div
-                                class="text-surface-700 dark:text-surface-100 text-2xl mb-8"
-                            >
-                                Please wait while we load your datasets.
-                            </div>
-                        </div>
+                    </template>
+                </Column>
+            </DataTable>
+        </template>
+    </Card>
+
+    <Card v-else-if="finished && !fileUploads.length">
+        <template #content>
+            <div
+                class="bg-surface-0 dark:bg-surface-950 px-6 py-20 md:px-12 lg:px-20"
+            >
+                <div class="text-surface-700 dark:text-surface-100 text-center">
+                    <div
+                        class="text-surface-900 dark:text-surface-0 font-bold text-5xl mb-4"
+                    >
+                        Upload your datasets.
                     </div>
-                </template>
-            </Card>
-        </div>
-    </div>
+                    <div
+                        class="text-surface-700 dark:text-surface-100 text-2xl mb-8"
+                    >
+                        You don't have any dataset yet. Start uploading your
+                        first dataset today.
+                    </div>
+                    <Button
+                        label="Upload"
+                        icon="bi-upload"
+                        class="mr-2"
+                        @click="route.push('/hermes/new')"
+                    ></Button>
+                </div>
+            </div>
+        </template>
+    </Card>
+
+    <Card v-else>
+        <template #content>
+            <div
+                class="bg-surface-0 dark:bg-surface-950 px-6 py-20 md:px-12 lg:px-20"
+            >
+                <div class="text-surface-700 dark:text-surface-100 text-center">
+                    <div
+                        class="text-surface-600 dark:text-surface-200 font-bold text-4xl mb-4"
+                    >
+                        Loading...
+                    </div>
+                    <div
+                        class="text-surface-700 dark:text-surface-100 text-2xl mb-8"
+                    >
+                        Please wait while we load your datasets.
+                    </div>
+                </div>
+            </div>
+        </template>
+    </Card>
 </template>
 
 <style scoped></style>
