27c1cb518f83dcec77fb377e8f3d3398eabc2eeb
Drew Hite <sagehen03@users.noreply.github.com>
2024-08-21 13:44:36 -0400

    Meta Analysis Graphs (#136)
    * some progress
    
    * UI working for meta analysis

--- pages/hermes/ma/[id].vue
+++ pages/hermes/ma/[id].vue
@@ -1,41 +1,78 @@
 <template>
-    <div class="grid">
-        <div class="col">
-            <Breadcrumb
-                :home="{ icon: 'bi-house', url: '/hermes/' }"
-                :model="[
-                    { label: 'Meta-Analyses', url: '/hermes/ma/' },
-                    { label: id },
-                ]"
-                class="mb-3"
+    <div class="grid" >
+      <div class="col text-center">
+        <Card>
+          <template #title>QQ Plot</template>
+          <template #content>
+            <Image
+                :src="`https://hermes-qc.s3.amazonaws.com/images/${id}/qq.png`"
+                alt="QQ Plot"
+                width="400"
+                preview
             />
-            <Card>
-                <template #content>
-                    <div class="surface-section px-4 py-8 md:px-6 lg:px-8">
-                        <div class="text-700 text-center">
-                            <div class="text-900 font-bold text-5xl mb-3">
-                                Create a meta analysis.
-                            </div>
-                            <div class="text-700 text-2xl mb-5">
-                                You don't have any meta analysis running. Start
-                                by creating a new analysis.
-                            </div>
-                            <Button
-                                label="Meta-Analysis"
-                                icon="bi-plus-lg"
-                                class="mr-2"
-                                @click="router.push('/hermes/ma/new')"
-                            ></Button>
-                        </div>
-                    </div>
-                </template>
-            </Card>
-        </div>
+          </template>
+        </Card>
+      </div>
+
+      <div class="col text-center">
+        <Card>
+          <template #title>Manhattan Plot</template>
+          <template #content>
+            <Image
+                :src="`https://hermes-qc.s3.amazonaws.com/images/${id}/manhattan.png`"
+                alt="Manhattan Plot"
+                width="400"
+                preview
+            />
+          </template>
+        </Card>
+      </div>
     </div>
+  <div class="col text-center">
+    <Card>
+      <template #title>Meta-Analysis Data</template>
+      <template #content>
+        <a :href="`${config.public.apiBaseUrl}/api/hermes-ma/results/${id}`"
+           target="_blank"
+           class="link-primary">
+          Download
+        </a>
+      </template>
+    </Card>
+  </div>
+  <div class="grid">
+    <div class="col col-md-12 mb-4">
+      <Card>
+        <template #title>Log Output</template>
+        <template #content>
+          <Shiki
+              :code="analysis.log"
+              lang="r"
+              :theme="'nord'"
+              style="
+                            height: 400px;
+                            overflow-y: scroll;
+                            white-space: pre-wrap;
+                        "
+          />
+        </template>
+      </Card>
+    </div>
+  </div>
+
 </template>
 <script setup>
 const route = useRoute();
-const router = useRouter();
-const maName = ref("Meta-Analysis");
 const id = route.params.id;
+
+import { useDatasetStore } from "~/stores/DatasetStore";
+const store = useDatasetStore();
+const analysis = ref({});
+const config = useRuntimeConfig();
+
+
+onMounted(async () => {
+  await store.fetchPhenotypes();
+  analysis.value = await store.fetchMetaAnalysis(id);
+});
 </script>
