323e739bf78ee5c5c99d61ec72e5ae354c29d670
Drew Hite <drew@drewhite.org>
2025-01-16 19:49:59 -0500

    pinia!

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -72,10 +72,6 @@ export default defineNuxtConfig({
       ],
   },
 
-  // https://stackoverflow.com/questions/74003458/cannot-find-module-pinia-dist-pinia-mjs-when-using-run-dev
-  alias: {
-      pinia: "/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs",
-  },
 
   telemetry: false,
 
